0003eb23c7e7fc1d3024eff4ded505205656925d
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-04-20 19:55:48 +0530

    jwt-middleware,generateToken

---
+++ server/jwt.js
@@ -0,0 +1,26 @@
+const jwt=require('jsonwebtoken');
+require('dotenv').config();
+
+
+const jwtAuthMiddleware=(req,res,next)=>{
+    const authheader=req.headers.authorization;
+    if(!authheader) return res.status(404).json({error:'unauthorized'});
+    try {
+        const token=authheader.split(" ")[1];
+        const decode=jwt.verify(token,process.env.JWT_SECRET);
+        req.user=decode;
+        next();
+    } catch (error) {
+        console.log("middleware error invalid token");
+        res.status(400).json({error:'internal server error'});
+    }
+}
+
+const generateToken=(userData)=>{
+    return jwt.sign(userData,process.env.JWT_SECRET);
+}
+
+module.exports={
+    jwtAuthMiddleware,
+    generateToken
+}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
-const port=3000;
+const port=3001;
 const db=require('./db');
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
 
 
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage!');
+  });
 
 
 
