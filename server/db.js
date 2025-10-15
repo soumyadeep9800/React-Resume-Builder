3a192cea899d61aa02ae3ae6d0761ab5edb55ef5
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-10-15 09:40:56 +0530

    add dockerfile and docker-compose.yml

--- server/db.js
+++ server/db.js
@@ -1,7 +1,10 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-// const mongoURL='mongodb://localhost:27017/resume-builder'; //local
-const mongoURL=process.env.DB_ATLAS_URL;
+//const mongoURL='mongodb://localhost:27017/resume-builder'; // for local db
+
+//const mongoURL='mongodb://admin:qwerty@mongo:27017/resume-builder?authSource=admin'; //for docker db
+
+const mongoURL=process.env.DB_ATLAS_URL; //global db
 
 mongoose.connect(mongoURL)
     .then(()=> console.log('connected to MongoDB server'))
