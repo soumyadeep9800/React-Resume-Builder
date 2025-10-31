b5a9cfd3dea496a063860b12f71077bcb0920cb4
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-10-31 14:48:33 +0530

    server gap

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
